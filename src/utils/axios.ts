1b8a6c821ab7f19081b8b3504073e0c2e47850bc
rafaelwildfire <rafaelwildfire@gmail.com>
2023-08-06 19:53:14 -0300

    fix: test req

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? "https://backend.aresprojetos.com.br/"
+    ? "http://18.191.144.188:8000/"
     : "http://localhost:8000/";
 
 console.log(process.env.NODE_ENV)
