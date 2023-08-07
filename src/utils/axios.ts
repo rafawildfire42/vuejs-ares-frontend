be5e2165250192ca12c49c9d60848a3b8af0e0ed
rafaelwildfire <rafaelwildfire@gmail.com>
2023-08-07 18:24:40 -0300

    fix: ajustando endereço para requisicao no axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? "http://18.191.144.188:8000/"
+    ? "https://api.aresprojetos.com.br/"
     : "http://localhost:8000/";
 
 console.log(process.env.NODE_ENV)
